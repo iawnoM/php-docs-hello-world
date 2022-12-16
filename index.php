0c8cd2c3f0df5ca0c0a2333b0679de386b5e2633
Riley <rcm2515@outlook.com>
2022-12-15 17:44:16 -0700

    add jokes files

--- index.php
+++ index.php
@@ -1,3 +1,50 @@
-<?php
+<html>
+    <head>
+        <title>Jokes Page</title>
+    </head>
 
-echo "Hello Azure!";
+    <body>
+    <h1>Jokes Page</h1>
+    <a href="loginPage.php">    LOGIN   </a><br>
+    <a> OR </a><br>
+    <a href="registerUser.php">REGISTER HERE</a><hr>
+    
+    <?php
+
+        // Check connection
+        include "dbConnect.php";
+
+
+        // include "getAllJokes.php";
+    
+    ?>
+
+    <!-- Get keyword from user -->
+    <form action="searchKeyword.php">
+        Enter keyword to search for:<br>
+        <input type="text" name="keyword"><br>
+
+        <input type="submit" value="Submit">
+    </form>
+    <hr>
+    <form action="addJoke.php">
+        Enter New Joke:<br>
+        <input type="text" name="newJoke"><br>
+        Answer: <br>
+        <input type="text" name="answer"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+
+    <?php
+        // Search Database for given word
+        // include "searchKeyword.php";        
+
+
+        $conn->close();
+    ?>
+
+    <a href = "bankingDemo/tester.php">BANKING DEMO</a>
+
+
+    </body>
+</html>
